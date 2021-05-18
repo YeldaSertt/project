ba80140d4e59a273ceb0b1dbde6b7b297ba389f3
esrasertdemir <esrasertdemir95@gmail.com>
2021-05-18 21:10:29 +0300

    change

--- book_store/book_store/book/models.py
+++ book_store/book_store/book/models.py
@@ -3,23 +3,23 @@ from django.db import models
 # Create your models here.
 class book_species(models.Model):
     species_id = models.BigAutoField(primary_key=True)
-    species_name = models.CharField( max_length=30,blank=True)
+    species_name = models.CharField( max_length=100,blank=True)
 
 class author(models.Model):
     author_id = models.BigAutoField(primary_key=True)
-    author_name = models.CharField(max_length=30,blank=False,verbose_name="Yazar Adı") 
-    author_lastname = models.CharField(max_length=30,blank=False,verbose_name="Yazarın Soyadı") 
+    author_name = models.CharField(max_length=100,blank=False,verbose_name="Yazar Adı") 
+    # author_lastname = models.CharField(max_length=100,blank=False,verbose_name="Yazarın Soyadı") 
 
 class book(models.Model):
     book_id = models.BigAutoField(primary_key=True)     
-    book_name = models.CharField(max_length=30,blank=False,verbose_name="Kitap Adı")
+    book_name = models.CharField(max_length=100,blank=False,verbose_name="Kitap Adı")
     sayfa_sayisi = models.IntegerField(blank=True)
     yayin_tarihi = models.DateTimeField(blank=True)
     species_id = models.ForeignKey(book_species,on_delete=models.CASCADE)
     author_id = models.ForeignKey(author,on_delete=models.CASCADE)
     image = models.CharField(max_length=200,blank=False)
     star = models.CharField(max_length=200,blank=False)
-    desc = models.CharField(max_length=500,blank=False)
+    desc = models.CharField(max_length=10000,blank=False)
 
 class comment(models.Model):
     comment_id = models.BigAutoField(primary_key=True) 
@@ -37,8 +37,7 @@ class persons(models.Model):
     person_name = models.CharField(max_length=100,blank=False)
     person_surname = models.CharField(max_length=100,blank=False)
     nick_name = models.CharField(max_length=100,blank=False)
-    description = models.CharField(max_length=100,blank=False)
-    description = models.CharField(max_length=100,blank=False)
+    description = models.CharField(max_length=10000,blank=False)
     photo = models.CharField(max_length=200,blank=False)
     pasword = models.CharField(max_length=200,blank=False)
     comment_id = models.ForeignKey(comment,on_delete=models.CASCADE)
--- book_store/book_store/book/views.py
+++ book_store/book_store/book/views.py
@@ -1,40 +1,67 @@
 
 from django.http.response import JsonResponse
 from django.shortcuts import render,HttpResponse,redirect
-from book.models import book,book_species,Author
+from book.models import book,book_species,author
 import json
 from django.core import serializers
 from datetime import date
+import dateparser
 
+def fileRead():
+    with open('book/book2.json', 'r') as f:
+        data=f.read()
+    obj = json.loads(data)
 
-def Db_save():
+    # obj = json.loads(file)
+    
+    databaseInsert(obj)
+#         record = Country(name = o)
+#         record.save()
+def databaseInsert(obj):
+    for o in obj:
+        species = book_species(species_name= o["tur"])
+        species.save()
+        author1 = author(author_name=o["author"])
+        author1.save()
+        book1 = book(book_name = o["book_name"],
+                            sayfa_sayisi = o["sayfa_sayisi"] if o["sayfa_sayisi"] else 0,
+                            yayin_tarihi= "2021-05-05",
+                            species_id=species,
+                            author_id = author1,
+                            image =o["images"][0] if o["images"] else "",
+                            star = str(o["star"]),
+                            desc = o["desc"]
+                            )
 
-    species1 = book_species(species_name="Fantastik")
-    species1.save()
-    species2 = book_species(species_name="Roman")
-    species2.save()
-    species3 = book_species(species_name="Macara")
-    species3.save()
+        book1.save()
+
+        # print(o)
+# def Db_save():
+
+#     species1 = book_species(species_name="Fantastik")
+#     species1.save()
+#     species2 = book_species(species_name="Roman")
+#     species2.save()
+#     species3 = book_species(species_name="Macara")
+#     species3.save()
    
-    book_author1 = Author(author_name="J.L",author_lastname="Rowling")
-    book_author1.save()
-    book_author2 = Author(author_name="Jules",author_lastname="Verne")
-    book_author2.save()
+#     book_author1 = Author(author_name="J.L",author_lastname="Rowling")
+#     book_author1.save()
+#     book_author2 = Author(author_name="Jules",author_lastname="Verne")
+#     book_author2.save()
    
-    book_author = book(name = "Harry Potther",sayfa_sayisi=500,yayin_tarihi=date(2005, 7, 27),book_species_id=species1,author_id=book_author1)
-    book_author.save()
-    book_author = book(name = "80 Günde Devri Alem",sayfa_sayisi=100,yayin_tarihi=date(2005, 7, 27),book_species_id=species3,author_id=book_author2)
-    book_author.save()
-    book_author = book(name = "Denizler Altında ",sayfa_sayisi=200,yayin_tarihi=date(2005, 7, 27),book_species_id=species2,author_id=book_author2)
-    book_author.save()
+#     book_author = book(name = "Harry Potther",sayfa_sayisi=500,yayin_tarihi=date(2005, 7, 27),book_species_id=species1,author_id=book_author1)
+#     book_author.save()
+#     book_author = book(name = "80 Günde Devri Alem",sayfa_sayisi=100,yayin_tarihi=date(2005, 7, 27),book_species_id=species3,author_id=book_author2)
+#     book_author.save()
+#     book_author = book(name = "Denizler Altında ",sayfa_sayisi=200,yayin_tarihi=date(2005, 7, 27),book_species_id=species2,author_id=book_author2)
+#     book_author.save()
 
     
 # Create your views here.
 def homepage(Request):
-
+    # fileRead()
     book_list = book.objects.all()
-    # for q in query:
-    #     print(q.name)
     # Db_save()
     return render(Request,'homepage.html',context = {"book_list":book_list})
 
