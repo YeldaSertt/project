067bf23fa5861797dcc69260fcf4bcc9e71fbbc1
YeldaaSert <yeldaasert@gmail.com>
2021-05-18 19:30:46 +0300

    database

--- book_store/book_store/book/models.py
+++ book_store/book_store/book/models.py
@@ -1,23 +1,55 @@
 from django.db import models
 
-
 # Create your models here.
 class book_species(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    species_id = models.BigAutoField(primary_key=True)
     species_name = models.CharField( max_length=30,blank=True)
 
-class Author(models.Model):
-    id = models.BigAutoField(primary_key=True)
+class author(models.Model):
+    author_id = models.BigAutoField(primary_key=True)
     author_name = models.CharField(max_length=30,blank=False,verbose_name="Yazar Adı") 
     author_lastname = models.CharField(max_length=30,blank=False,verbose_name="Yazarın Soyadı") 
 
 class book(models.Model):
-    id = models.BigAutoField(primary_key=True)     
-    name = models.CharField(max_length=30,blank=False,verbose_name="Kitap Adı")
+    book_id = models.BigAutoField(primary_key=True)     
+    book_name = models.CharField(max_length=30,blank=False,verbose_name="Kitap Adı")
     sayfa_sayisi = models.IntegerField(blank=True)
     yayin_tarihi = models.DateTimeField(blank=True)
-    book_species_id = models.ForeignKey(book_species,on_delete=models.CASCADE)
-    author_id = models.ForeignKey(Author,on_delete=models.CASCADE)
+    species_id = models.ForeignKey(book_species,on_delete=models.CASCADE)
+    author_id = models.ForeignKey(author,on_delete=models.CASCADE)
+    image = models.CharField(max_length=200,blank=False)
+    star = models.CharField(max_length=200,blank=False)
+    desc = models.CharField(max_length=500,blank=False)
+
+class comment(models.Model):
+    comment_id = models.BigAutoField(primary_key=True) 
+    book_id =models.ForeignKey(book,on_delete=models.CASCADE)      
+    description = models.CharField(max_length=100,blank=False,verbose_name="Kitap Adı")
+    comment_admit = models.IntegerField(blank=True)
+
+class authority(models.Model):
+    yetki_id = models.BigAutoField(primary_key=True)
+    admin = models.CharField(max_length=100,blank=False)
+    users = models.CharField(max_length=100,blank=False)
+
+class persons(models.Model):
+    person_id = models.BigAutoField(primary_key=True)
+    person_name = models.CharField(max_length=100,blank=False)
+    person_surname = models.CharField(max_length=100,blank=False)
+    nick_name = models.CharField(max_length=100,blank=False)
+    description = models.CharField(max_length=100,blank=False)
+    description = models.CharField(max_length=100,blank=False)
+    photo = models.CharField(max_length=200,blank=False)
+    pasword = models.CharField(max_length=200,blank=False)
+    comment_id = models.ForeignKey(comment,on_delete=models.CASCADE)
+    yetki_id = models.ForeignKey(authority,on_delete=models.CASCADE)
+
+
+    
+    
+
+
+
     # book = models.OneToOneField(book_species,on_delete=models.CASCADE,primary_key=True)  
     
 
