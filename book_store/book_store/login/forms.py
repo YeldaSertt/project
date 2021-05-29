af0a92bda3a439322d5a95c4e1943812e8db462b
YeldaaSert <83091473+YeldaSertt@users.noreply.github.com>
2021-05-30 01:12:36 +0300

    updaate

--- book_store/book_store/login/forms.py
+++ book_store/book_store/login/forms.py
@@ -1,3 +1,4 @@
+
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import fields
@@ -6,3 +7,10 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields=['first_name','last_name','username','email','password']
+
+
+
+    def __init__(self,*args,**kwargs):
+        super(RegisterForm,self).__init__(*args,**kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs = {'class':'form-control'}
