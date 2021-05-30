6a52786261f0d8a23ee6d9c9bf76487827639aaa
YeldaaSert <83091473+YeldaSertt@users.noreply.github.com>
2021-05-30 19:37:07 +0300

    update

--- book_store/book_store/login/views.py
+++ book_store/book_store/login/views.py
@@ -21,7 +21,7 @@ def register(Request):
             if user.is_active:
                 login(Request)
                 messages.success(Request,'<b> Tebrikler başarılı </b>',extra_tags='success')
-                # return HttpResponseRedirect(reverse('homepage'))
+                return HttpResponseRedirect(reverse('homepage'))
     return render(Request,'auths/register.html',context={'form':form})
     
 def login(request):
